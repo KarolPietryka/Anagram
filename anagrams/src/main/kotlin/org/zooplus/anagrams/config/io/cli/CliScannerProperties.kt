f2e2c2d5ba4b339fc36eaa27791e28d15f839a56
Karol Pietryka <karol.pietryka11@gmail.com>
2023-06-28 22:41:01 +0200

    [Refactor]Move CliScannerProperties

--- anagrams/src/main/kotlin/org/zooplus/anagrams/config/io/cli/CliScannerProperties.kt
+++ anagrams/src/main/kotlin/org/zooplus/anagrams/config/io/cli/CliScannerProperties.kt
@@ -1,4 +1,4 @@
-package org.zooplus.anagrams.config
+package org.zooplus.anagrams.config.io.cli
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
