91118cfa4a7578bfe7cb7a95da1cdc057a4ec577
Karol Pietryka <karol.pietryka11@gmail.com>
2023-06-30 18:56:19 +0200

    [Add]Add mechanism of preparation resources for SortedDictionaryAnagramSearcher

---
+++ anagrams/src/main/kotlin/org/zooplus/anagrams/service/boot/SortDictionary.kt
@@ -0,0 +1,28 @@
+package org.zooplus.anagrams.service.boot
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Profile
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.zooplus.anagrams.component.io.resources.dictionary.reader.DictionaryReader
+import org.zooplus.anagrams.component.io.resources.dictionary.writter.DictionaryWriter
+import org.zooplus.anagrams.config.props.io.resources.dictionary.DictionaryProperties
+
+@Component
+@Order(1)
+@ConditionalOnProperty(value = ["app.zooplus.anagram.searcher.type"], havingValue = "sorted")
+@Profile("!test")
+class SortDictionary(
+    private val dictionaryReader: DictionaryReader,
+    @Qualifier("sortedDictionaryWriter")
+    private val dictionaryWriter: DictionaryWriter,
+    private val dictionaryProperties: DictionaryProperties,
+): ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        val sortedContent = dictionaryReader.getFromDirectory().dictionaryContent.sorted()
+        dictionaryWriter.write(sortedContent, dictionaryProperties.sortedDictionaryFileName)
+    }
+}
---
+++ anagrams/src/test/kotlin/org/zooplus/anagrams/service/boot/SortDictionaryTest.kt
@@ -0,0 +1,70 @@
+package org.zooplus.anagrams.service.boot
+
+import com.google.common.jimfs.Configuration
+import com.google.common.jimfs.Jimfs
+import org.junit.jupiter.api.AfterEach
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.mockito.Mock
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Qualifier
+import org.zooplus.anagrams.component.io.resources.dictionary.reader.DictionaryReader
+import org.zooplus.anagrams.component.io.resources.dictionary.writter.DictionaryWriter
+import org.zooplus.anagrams.config.props.io.resources.dictionary.DictionaryProperties
+import java.nio.file.FileSystem
+import java.nio.file.Files
+import java.nio.file.Path
+
+
+internal class SortDictionaryTest {
+
+    private lateinit var fs: FileSystem
+    private lateinit var dirPath: Path
+
+    @BeforeEach
+    fun setup() {
+        fs = Jimfs.newFileSystem(Configuration.unix())
+        dirPath = fs.getPath("/test")
+
+        Files.createDirectory(dirPath)
+    }
+    @AfterEach
+    fun teardown() {
+        fs.close()
+    }
+
+    @Mock
+    private lateinit var dictionaryReader: DictionaryReader
+    @Mock
+    private lateinit var dictionaryWriter: DictionaryWriter
+    @Mock
+    private lateinit var dictionaryProperties: DictionaryProperties
+    //@Test
+    fun `should return all words from all files in the directory`() {
+        val dirPath = fs.getPath("/test")
+        val file1 = dirPath.resolve("file1.txt")
+        val file2 = dirPath.resolve("file2.txt")
+
+        Files.write(file1, listOf("hello", "world"))
+        Files.write(file2, listOf("test", "input"))
+
+        whenever(dictionaryProperties.dictionaryDirPath).thenReturn(dirPath.toString())
+        whenever(dictionaryProperties.sortedDictionaryFileName).thenReturn("sorted.txt")
+
+
+        SortDictionary(dictionaryReader, dictionaryWriter, dictionaryProperties).run(null)
+
+
+        val expectedSortedContent = listOf("hello", "input", "test", "world")
+        val sortedContent = Files.readAllLines(dirPath.resolve("sorted.txt"))
+
+        assertEquals(expectedSortedContent, sortedContent)
+        verify(dictionaryReader).getFromDirectory()
+        verify(dictionaryWriter).write(expectedSortedContent,"sorted.txt" )
+
+    }
+
+}
