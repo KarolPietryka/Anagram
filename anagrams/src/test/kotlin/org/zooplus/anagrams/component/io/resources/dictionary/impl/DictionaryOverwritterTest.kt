91f21be3b0a6ba089bdd5b5bae52df4b052facee
Karol Pietryka <karol.pietryka11@gmail.com>
2023-06-30 18:39:05 +0200

    [Add]Add test for DictionaryOverwritter implementation

---
+++ anagrams/src/test/kotlin/org/zooplus/anagrams/component/io/resources/dictionary/impl/DictionaryOverwritterTest.kt
@@ -0,0 +1,67 @@
+package org.zooplus.anagrams.component.io.resources.dictionary.impl
+
+import com.google.common.jimfs.Configuration
+import com.google.common.jimfs.Jimfs
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.zooplus.anagrams.component.io.resources.dictionary.writter.impl.DictionaryOverwritter
+import org.zooplus.anagrams.config.props.io.resources.dictionary.DictionaryProperties
+import java.nio.file.FileSystem
+import java.nio.file.Files
+import java.nio.file.Path
+
+internal class DictionaryOverwritterTest {
+
+    private lateinit var fs: FileSystem
+    private lateinit var dirPath: Path
+
+    @BeforeEach
+    fun setup() {
+        fs = Jimfs.newFileSystem(Configuration.unix())
+        dirPath = fs.getPath("/test")
+
+        Files.createDirectory(dirPath)
+    }
+    @AfterEach
+    fun teardown() {
+        fs.close()
+    }
+    @Test
+    fun write() {
+        val dirPath = fs.getPath("/test")
+
+        val file1 = dirPath.resolve("file1.txt")
+        val file2 = dirPath.resolve("file2.txt")
+
+        Files.write(file1, listOf("hello", "world"))
+        Files.write(file2, listOf("test", "input"))
+
+        val dictionaryProperties: DictionaryProperties = mock()
+        whenever(dictionaryProperties.dictionaryPath).thenReturn(dirPath.toString())
+
+        DictionaryOverwritter(fs, dictionaryProperties).write(
+            listOf("hello", "input", "test", "world"),
+            "sorted.txt"
+        )
+
+        val expectedSortedContent = listOf("hello", "input", "test", "world")
+        val sortedContent = Files.readAllLines(dirPath.resolve("/test/sorted.txt"))
+
+        assertEquals(expectedSortedContent, sortedContent)
+        assertEquals(1, countFilesInDirectory(dirPath))
+    }
+
+    private fun countFilesInDirectory(directoryPath: Path): Int {
+        return Files.list(directoryPath)
+            .count()
+            .toInt()
+    }
+}
