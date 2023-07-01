f0b8ad85edce344a4be518d872090027ddaa756a
Karol Pietryka <karol.pietryka11@gmail.com>
2023-07-01 10:29:18 +0200

    [Improvement]Improvement on search result for the user

--- anagrams/src/main/kotlin/org/zooplus/anagrams/component/io/cli/CliScanner.kt
+++ anagrams/src/main/kotlin/org/zooplus/anagrams/component/io/cli/CliScanner.kt
@@ -16,7 +16,7 @@ class CliScanner constructor(
             println("Enter world for anagram")
             val input = scanner.nextLine()
             anagramSearcher.getAnagram(input)?.let {
-                println("Anagram of word $input found in dictionaries is $it")
+                println("The longest anagram is of word \"$input\" found in dictionaries is \"$it\"")
             } ?: println("No anagram of word $input found")
         }
     }
